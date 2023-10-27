23eaad0629f36e4fe6e860426961b146b6035519
Sarathraj K <sarathrajk@techcedence.com>
2023-10-27 15:48:03 +0530

    Installing Chakra UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
 
+import './App.css'
+import { Button, ButtonGroup } from '@chakra-ui/react'
 function App() {
   
 
-
-  
+return <Button   colorScheme='green'>Button</Button>
 }
 
 export default App
