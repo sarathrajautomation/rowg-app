c0abcb20d803176e5b05507c7e20e2b18da4a1d6
Sarathraj K <sarathrajk@techcedence.com>
2023-10-27 19:31:04 +0530

    theme configuration

--- src/component/NavBar.tsx
+++ src/component/NavBar.tsx
@@ -1,13 +1,13 @@
-import { HStack, Image } from '@chakra-ui/react'
-import logo from '../assets/logo.webp'
+import { HStack, Image, Text } from "@chakra-ui/react";
+import logo from "../assets/logo.webp";
 
 const NavBar = () => {
   return (
     <HStack>
-<Image src={logo}  boxSize='60px'/>
-
+      <Image src={logo} boxSize="60px" />
+      <Text>NavBar</Text>
     </HStack>
-  )
-}
+  );
+};
 
-export default NavBar
+export default NavBar;
