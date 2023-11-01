5eb936a3da434d4d28d4d80269dad175faae1c02
Sarathraj K <sarathrajk@techcedence.com>
2023-11-01 18:27:44 +0530

    Implemented the Platform Icons

--- src/component/PlatformIcon.tsx
+++ src/component/PlatformIcon.tsx
@@ -1,17 +1,38 @@
-import React from 'react'
-import { Platform } from '../hooks/useGames'
-import { Text } from '@chakra-ui/react';
-interface props{
-    platform: Platform[];
-}
-const PlatformIcon = ({platform}:props) => {
-  return (
-   <>
-   
-   {platform.map((platform)=><Text>{platform.name}</Text>)}
-   
-   </>
-  )
-}
+import {
+  FaWindows,
+  FaPlaystation,
+  FaXbox,
+  FaApple,
+  FaLinux,
+  FaAndroid,
+} from "react-icons/fa";
+import { MdPhoneIphone } from "react-icons/md";
 
-export default PlatformIcon
+import { Platform } from "../hooks/useGames";
+import { HStack, Icon, Text } from "@chakra-ui/react";
+import { BsGlobe } from "react-icons/bs";
+import { IconType } from "react-icons";
+interface props {
+  platform: Platform[];
+}
+const PlatformIcon = ({ platform }: props) => {
+  const iconMap: { [key: string]: IconType } = {
+    pc: FaWindows,
+    playstation: FaPlaystation,
+    xbox: FaXbox,
+    mac: FaApple,
+    linux: FaLinux,
+    ios: MdPhoneIphone,
+    web: BsGlobe,
+    android: FaAndroid,
+  };
+  return (
+    <HStack marginY={1}>
+      {platform.map((platform) => (
+        <Icon as={iconMap[platform.slug]} color='grey.500' />
+      ))}
+    </HStack>
+  );
+};
+
+export default PlatformIcon;
